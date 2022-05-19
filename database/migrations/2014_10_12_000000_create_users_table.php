b87040265c14e59a80cc16458ce0eef91bb34d84
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-19 21:33:02 +0200

    database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('password');
+            $table->id('rank');
+            $table->id('group');
             $table->rememberToken();
             $table->timestamps();
         });
