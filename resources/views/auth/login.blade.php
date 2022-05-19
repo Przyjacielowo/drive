a28018d31a9a77f68cdcc24872a4a0f3042779a4
Prymex6 <bartek.prymka02@gmail.com>
2022-05-19 21:58:37 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,9 +44,9 @@
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
+                                    {{-- <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
-                                    </label>
+                                    </label> --}}
                                 </div>
                             </div>
                         </div>
