e6f9b0d231b1276ea5105f710cccda8c7eef3858
Prymex6 <bartek.prymka02@gmail.com>
2022-05-19 21:55:53 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,9 @@ Route::group(['middleware' => 'auth'], function () {
   Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
-Auth::routes();
+Auth::routes([
+  'register' => false,
+  'reset' => false, 
+  'verify' => false, 
+]);
 
