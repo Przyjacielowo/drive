2a87db9a2fd342a374b9f9e26e4e169c9e424f8f
Prymex6 <bartek.prymka02@gmail.com>
2022-05-19 21:40:29 +0200

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
-            $table->string('last_name')->unique();
+            $table->string('last_name');
             $table->string('password');
             $table->id('rank');
             $table->id('group');
