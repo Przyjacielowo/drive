08b7f5566cbaf851a0828dbe1cd9626810abb676
Prymex6 <bartek.prymka02@gmail.com>
2022-05-19 21:38:48 +0200

    test

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,16 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name');
+            $table->string('last_name')->unique();
             $table->string('password');
-<<<<<<< HEAD
             $table->id('rank');
             $table->id('group');
-=======
-            $table->rememberToken();
->>>>>>> parent of b870402 (database)
             $table->timestamps();
         });
     }
