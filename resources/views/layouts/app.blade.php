f6a095be927d6e40523b196aa524b5c75515e263
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-20 22:03:02 +0200

    logout fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -160,14 +160,6 @@
         </li>
         </ul>
     </nav>
-    
-    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-    Logout
-</a>
-
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-    {{ csrf_field() }}
-</form>
 
 
         <main class="py-4">
