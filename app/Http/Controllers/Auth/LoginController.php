47a07b95f31ec237319e2301faab23289fff1481
Prymex6 <bartek.prymka02@gmail.com>
2022-05-20 21:43:04 +0200

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,7 +43,7 @@ class LoginController extends Controller
     public function login(Request $request)
     {   
 
-        dd($request);
+        dd($request->all());
 
         $this->validate($request, [
             'login' => 'required',
