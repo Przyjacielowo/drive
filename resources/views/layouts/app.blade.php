3d91a9144a11d002a494fc5b681b6ee534b28580
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-21 22:57:10 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
 </head>
 <body>
-        
+        <main>
+            @yield('content')
+        </main>
+
 
 </body>
 </html>
