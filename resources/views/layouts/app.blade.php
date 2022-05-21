aaa38c3541543e0cc3efc4469625b190d2ef86e1
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-21 22:46:59 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
         <ul class="navbar-nav">
             @guest
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="https://drive.ridiculam.pl" class="nav-link" style="font-weight: 700; font-size: 22px;">drive.ridiculam.pl</a>
+                <a href="{{ route('home') }}" class="nav-link" style="font-weight: 700; font-size: 22px;">drive.ridiculam.pl</a>
             </li>
             @endguest 
             @auth
@@ -37,10 +37,10 @@
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="https://drive.ridiculam.pl" class="nav-link">Home</a>
+                <a href="{{ route('home') }}" class="nav-link">Home</a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="#" class="nav-link">Contact</a>
+                <a href="{{ route('home') }}" class="nav-link">Contact</a>
             </li>
             @endauth
         </ul>
