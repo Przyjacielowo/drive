e0aa5e9c7795e50295bec6cedf791a33f7aca57d
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-21 13:17:32 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,11 +64,8 @@
     <div class="sidebar">
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block" style="display: block; text-align: center;">Administrator</a>
         </div>
       </div>
 
