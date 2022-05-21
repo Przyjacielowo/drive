10a0047675f779f04c32ac90c5677ec1faa1b24d
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-21 13:40:17 +0200

    add plugins file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -734,5 +734,16 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+
+
 </body>
 </html>
