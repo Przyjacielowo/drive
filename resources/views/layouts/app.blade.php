a4e3004b33f50f87886fdf85f47a0932844d4eda
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-21 21:37:57 +0200

    catalog create

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,33 +123,12 @@
                 </ul>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-copy"></i>
-                <p>
-                    Ulubione katalogi
-                    <i class="right fas fa-angle-left"></i>
-                </p>
+                <a href="{{ route('catalog.create', create) }}" class="nav-link active">
+                    <i class="nav-icon fas fa-copy"></i>
+                    <p>
+                        Dodaj katalog
+                    </p>
                 </a>
-                <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="../../index.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>katalog v1</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="../../index2.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>katalog v2</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="../../index3.html" class="nav-link">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>katalog v3</p>
-                    </a>
-                </li>
-                </ul>
             </li>
             
         </nav>
