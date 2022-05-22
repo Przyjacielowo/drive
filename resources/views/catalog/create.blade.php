7f43b4ec573e80a97b3fa967e6cf7dc36eca4cd1
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-22 13:01:27 +0200

    Update create.blade.php

--- resources/views/catalog/create.blade.php
+++ resources/views/catalog/create.blade.php
@@ -1,29 +1,30 @@
-@extends('home')
-<div class="content-wrapper">
-    <div class="col-md-6">
-        <div class="card card-primary">
-            <div class="card-header">
-                <h3 class="card-title">Stwórz katalog</h3>
+<div>
+    @extends('home')
+    <div class="content-wrapper">
+        <div class="col-md-6">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Stwórz katalog</h3>
+                </div>
+
+
+                <form>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label for="catalog_name">Nazwa katalogu</label>
+                            <input type="email" class="form-control" id="catalog_name">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1">
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Dodaj</button>
+                    </div>
+                </form>
             </div>
-
-
-            <form>
-                <div class="card-body">
-                    <div class="form-group">
-                        <label for="catalog_name">Nazwa katalogu</label>
-                        <input type="email" class="form-control" id="catalog_name">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
-                    </div>
-                </div>
-
-                <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Dodaj</button>
-                </div>
-            </form>
-        </div>
-    </div> 
+        </div> 
+    </div>
 </div>
-
