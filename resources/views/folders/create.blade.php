669d9bea8f1fc85ab19697dd16433bf71b9a528f
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-22 21:26:47 +0200

    Update create.blade.php

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -35,4 +35,8 @@
             </div>
         </div> 
     </div>
+    <div class="custom-file">
+  <input type="file" class="custom-file-input" id="customFileLang" lang="pl-Pl">
+  <label class="custom-file-label" for="customFileLang">Wybierz pliki</label>
+</div>
 @endsection
