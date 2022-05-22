884ebf28853aeadccddca299af256e834cfb557b
Prymex6 <bartek.prymka02@gmail.com>
2022-05-22 20:52:57 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-    Route::resource('folders', [App\Http\Controllers\FoldersController::class]);
+    Route::resource('folders', App\Http\Controllers\FoldersController::class);
 });
 
 Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'index'])->name('login');
