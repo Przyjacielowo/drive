66a9545a76405fc43ecf817d960eb816f501cb9b
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-22 21:12:36 +0200

    Update create.blade.php

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="content-wrapper" style="margin-top: 20px;">
-        <div class="col-md-6">
+        <div class="col-md-8" style="margin: 0 auto;">
             <div class="card card-primary">
                 <div class="card-header">
                     <h3 class="card-title">Dodaj folder</h3>
@@ -18,6 +18,18 @@
                         <div class="form-group">
                             <label>Opis</label>
                             <textarea class="form-control" rows="3"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputFile">Plik wejściowy</label>
+                            <div class="input-group">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="exampleInputFile">
+                                    <label class="custom-file-label" for="exampleInputFile">Wybierz plik</label>
+                                </div>
+                                <div class="input-group-append">
+                                    <span class="input-group-text">Załaduj</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
