6289c75ad243c24f60746c77d6b02fddc6515936
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-22 22:06:57 +0200

    Update create.blade.php

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -20,17 +20,17 @@
                             <textarea class="form-control" rows="3"></textarea>
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputFile">Plik wejściowy</label>
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" id="customFileLang" lang="pl-Pl" multiple>
-                                <label class="custom-file-label" for="customFileLang">Wybierz pliki</label>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label class="custom-file-upload" style="border: 1px solid #ccc; display: inline-block; padding: 6px 12px; cursor: pointer; font-weight: 500; width: 100%; border-radius: 5px;">
-                                <input type="file" style="display: none;"/>
+                            <label class="custom-file-upload" style="border: 1px solid #ccc; display: inline-block; padding: 6px 12px; cursor: pointer; font-weight: 400; width: 100%; border-radius: 5px;">
+                                <input type="file" style="display: none;" multiple/>
                                 Wybierz plik
                             </label>
+                        </div>
+                        <div class="form-group">
+                            <div id="yourBtn" style="height: 50px; width: 100px;border: 1px dashed #BBB; cursor:pointer;" onclick="getFile()">Click to upload!</div>
+                            <!-- this is your file input tag, so i hide it!-->
+                            <div style='height: 0px;width:0px; overflow:hidden;'><input id="upfile" type="file" value="upload"/></div>
+                            <!-- here you can have file submit button or you can write a simple script to upload the file automatically-->
+                            <input type="submit" value='submit' >
                         </div>
                     </div>
 
