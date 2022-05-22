ef9ed9765b88771fa693c01985d90ab2e02b4ec0
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-22 22:50:28 +0200

    Update create.blade.php

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -20,7 +20,7 @@
                             <textarea class="form-control" rows="3"></textarea>
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputFile">Plik wejściowy</label>
+                            
                             <div class="input-group">
                                 <div class="custom-file">
                                     <input type="file" class="custom-file-input" id="exampleInputFile">
@@ -32,21 +32,11 @@
                             </div>
                         </div>
                         <div class="form-group">
+                            <label for="file-upload">Plik wejściowy</label>
                             <label for="file-upload" class="custom-file-upload" style="border: 1px solid #ccc; display: inline-block; padding: 6px 12px; cursor: pointer; font-weight: 400; width: 100%; border-radius: 5px;">
                                 Wybierz plik
                             </label>
                             <input id="file-upload" type="file" style="display: none;" multiple/>
-                        </div>
-                        <div class="form-group">
-                            <input type="file" name="file" id="file" class="inputfile" data-multiple-caption="{count} files selected" multiple style="width: 0.1px; height: 0.1px; opacity: 0; overflow: hidden; position: absolute; z-index: -1;"/>
-                            <label for="file" style="font-size: 1.25em; font-weight: 700; color: white; background-color: black; display: inline-block; cursor: pointer;">Wybierz plik</label>
-                        </div>
-                        <div class="form-group">
-                            <div class="box">
-					            <input type="file" name="file-1[]" id="file-1" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" multiple="">
-					            <label for="file-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg>        
-                                <span>Choose a file…</span></label>
-				            </div>
                         </div>
                     </div>
 
