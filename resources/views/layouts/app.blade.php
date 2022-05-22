b1eb834b59923ed6316fbe6813b66540904a7c95
Prymex6 <bartek.prymka02@gmail.com>
2022-05-22 21:42:43 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,7 +93,7 @@
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
                     with font-awesome or any other icon font library -->
-                <li class="nav-item menu-is-opening menu-open">
+                {{-- <li class="nav-item menu-is-opening menu-open">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-copy"></i>
                         <p>
@@ -121,7 +121,7 @@
                         </a>
                     </li>
                     </ul>
-                </li>
+                </li> --}}
                 <li class="nav-item">
                     <a href="{{ route('folders.create') }}" class="nav-link active">
                         <i class="nav-icon fas fa-copy"></i>
