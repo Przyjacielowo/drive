52e5644057ba44ea7f6e9f58f12038e8f0ce4bf1
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-23 21:42:45 +0200

    Update create.blade.php

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -58,12 +58,12 @@
             </div>
         </div> 
     </div>
+    <script>
+        let inputFile = document.getElementById('file-input');
+        let fileNameField = document.getElementById('file-name');
+        inputFile.addEventListener('change', function(event){
+            let uploadedFileName = event.target.files[0].name;
+            fileNameField.textContent = uploadedFileName;
+        })
+    </script>   
 @endsection
-<script>
-    let inputFile = document.getElementById('file-input');
-    let fileNameField = document.getElementById('file-name');
-    inputFile.addEventListener('change', function(event){
-        let uploadedFileName = event.target.files[0].name;
-        fileNameField.textContent = uploadedFileName;
-    })
-</script>
