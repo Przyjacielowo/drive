0ad7e62cf0192228042855e3c3fd699211e07129
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-23 22:36:47 +0200

    Update create.blade.php

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -20,10 +20,10 @@
                             <textarea class="form-control" rows="3"></textarea>
                         </div>
                         <div class="form-group">
-                            <label for="file-upload__input">Plik wejściowy</label><br>
-                                <input class="file-upload__input" type="file" name="myFile[]" id="myFile" style="display: none;" multiple>
-                                <button class="file-upload__button" type="button" style="-webkit-appearance: none; color: #fff; background-color: #007bff; border-color: #007bff; box-shadow: none; border-radius: 4px; outline: none; padding: 0.375rem 0.75rem; margin-right: 7px; font-size: 1em; cursor: pointer; font-weight: 400;">Wybierz pliki</button>
-                                <span class="file-upload__label" sttyle="max-width: 250px; font-size: 0.95em; text-overflow: ellipsis; overflow: hidden; white-space: nowrap;"></span>
+                            <label for="file-upload__input">Plik/i wejściowy/e</label><br>
+                            <input class="file-upload__input" type="file" name="myFile[]" id="myFile" style="display: none;" multiple>
+                            <button class="file-upload__button" type="button" style="-webkit-appearance: none; color: #fff; background-color: #007bff; border-color: #007bff; box-shadow: none; border-radius: 4px; outline: none; padding: 0.375rem 0.75rem; margin-right: 7px; font-size: 1em; cursor: pointer; font-weight: 400;">Wybierz plik/i</button>
+                            <span class="file-upload__label" sttyle="max-width: 250px; font-size: 0.95em; text-overflow: ellipsis; overflow: hidden; white-space: nowrap;"></span>
                         </div>
                     </div>
 
