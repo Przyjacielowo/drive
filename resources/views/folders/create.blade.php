684074e5ab26570ebcbb5e59b4552a701a3850a0
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-23 21:10:31 +0200

    Update create.blade.php

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -19,25 +19,9 @@
                             <label>Opis</label>
                             <textarea class="form-control" rows="3"></textarea>
                         </div>
-                        <div class="form-group">
-                            
-                            <div class="input-group">
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="exampleInputFile">
-                                    <label class="custom-file-label" for="exampleInputFile">zwrot butów.PNG</label>
-                                </div>
-                                <div class="input-group-append">
-                                    <span class="input-group-text">Upload</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="file-upload">Plik wejściowy</label>
-                            <label for="file-upload" class="custom-file-upload" style="border: 1px solid #ccc; display: inline-block; padding: 6px 12px; cursor: pointer; font-weight: 400; width: 100%; border-radius: 5px;">
-                                Wybierz plik
-                            </label>
-                            <input id="file-upload" type="file" style="display: none;" multiple/>
-                        </div>
+                        
+                        <label class="form-label" for="customFile">Default file input example</label>
+                        <input type="file" class="form-control" id="customFile" />
                     </div>
 
                     <div class="card-footer">
