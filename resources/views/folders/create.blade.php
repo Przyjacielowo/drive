a61a063cf95fa0bc176d4ef0838c51c72d89612b
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-23 21:22:54 +0200

    Update create.blade.php

--- resources/views/folders/create.blade.php
+++ resources/views/folders/create.blade.php
@@ -20,8 +20,33 @@
                             <textarea class="form-control" rows="3"></textarea>
                         </div>
                         
-                        <input type="file" name="file" id="file" class="inputfile" style="width: 0.1px; height: 0.1px; opacity: 0; overflow: hidden; position: absolute; z-index: -1; font-size: 1.25em; font-weight: 700;     color: white; background-color: black; display: inline-block; background-color: red; cursor: pointer;"/>
+                        <input type="file" name="file" id="file" class="inputfile" data-multiple-caption="{count} files selected" multiple="" style="width: 0.1px; height: 0.1px; opacity: 0; overflow: hidden; position: absolute; z-index: -1; font-size: 1.25em; font-weight: 700;     color: white; background-color: black; display: inline-block; background-color: red; cursor: pointer;"/>
                         <label for="file" style="font-size: 1.25em; font-weight: 700; color: white; background-color: black; display: inline-block; background-color: red; cursor: pointer;">Choose a file</label>
+                        <script>
+                            var inputs = document.querySelectorAll( '.inputfile' );
+                            Array.prototype.forEach.call( inputs, function( input )
+                            {
+                                var label	 = input.nextElementSibling,
+                                    labelVal = label.innerHTML;
+
+                                input.addEventListener( 'change', function( e )
+                                {
+                                    var fileName = '';
+                                    if( this.files && this.files.length > 1 )
+                                        fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+                                    else
+                                        fileName = e.target.value.split( '\' ).pop();
+
+                                    if( fileName )
+                                        label.querySelector( 'span' ).innerHTML = fileName;
+                                    else
+                                        label.innerHTML = labelVal;
+                                });
+                            });
+
+
+
+                        </script>
                     </div>
 
                     <div class="card-footer">
