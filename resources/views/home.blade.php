e2d8aac55ac125cd806d10b882cdf3a09cdf56b9
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-24 22:43:30 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,15 @@
 
 @section('content')
 
-    
+	<div class="content-wrapper">
+		<div class="folder_container">
+			<i class="fa-solid fa-folder"></i>
+		</div>
+		
+
+
+
+	</div>
 
     
 @endsection
