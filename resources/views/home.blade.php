7367a9077e25e48ade67fe20bdfee30bb52a2ef4
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-24 22:49:22 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,9 @@
 
 	<div class="content-wrapper">
 		<div class="folder_container">
-			<i class="fa-solid fa-folder" style=""></i>
+			<div class="folder_icon">
+				<i class="fa-solid fa-folder"></i>
+			</div>
 			<span>Nazwa folderu.pl</span>
 		</div>
 		
