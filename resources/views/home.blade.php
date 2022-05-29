e090b27b11df491e18fff9078671c4a732fdf182
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-29 22:23:42 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,18 @@
 @section('content')
 
 	<div class="margin-content-wraper content-wrapper">
-		<div class="folder_container">
+		<!-- <div class="folder_container">
 			<div class="folder_icon">
 				<i class="fa-solid fa-folder fa-10x"></i>
 			</div>
 			<span>Nazwa folderu.pl</span>
-		</div>
+		</div> -->
 		
+		<div class="image_container">
+			<div class="image">
+				<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+			</div>
+		</div>
 
 
 
