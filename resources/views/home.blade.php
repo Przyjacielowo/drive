0fda0e90516d356638cb32951aebd3f98d547245
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-29 23:31:12 +0200

    images styles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,107 +9,107 @@
 			</div>
 			<span>Nazwa folderu.pl</span>
 		</div> -->
-		
-		<div class="image_container">
-			<div class="image">
-				<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+		<div class="images_container">
+			<div class="image_container">
+				<div class="image">
+					<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+				</div>
+				<div class="image_name">
+					<span>DSC00212.JPG</span>
+				</div>
+				<div class="image_delete">
+						<i class="fa-solid fa-trash-can"></i>
+				</div>
 			</div>
-			<div class="image_name">
-				<span>DSC00212.JPG</span>
+			<div class="image_container">
+				<div class="image">
+					<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+				</div>
+				<div class="image_name">
+					<span>DSC00212.JPG</span>
+				</div>
+				<div class="image_delete">
+						<i class="fa-solid fa-trash-can"></i>
+				</div>
 			</div>
-			<div class="image_delete">
-					<i class="fa-solid fa-trash-can"></i>
+			<div class="image_container">
+				<div class="image">
+					<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+				</div>
+				<div class="image_name">
+					<span>DSC00212.JPG</span>
+				</div>
+				<div class="image_delete">
+						<i class="fa-solid fa-trash-can"></i>
+				</div>
+			</div>
+			<div class="image_container">
+				<div class="image">
+					<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+				</div>
+				<div class="image_name">
+					<span>DSC00212.JPG</span>
+				</div>
+				<div class="image_delete">
+						<i class="fa-solid fa-trash-can"></i>
+				</div>
+			</div>
+			<div class="image_container">
+				<div class="image">
+					<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+				</div>
+				<div class="image_name">
+					<span>DSC00212.JPG</span>
+				</div>
+				<div class="image_delete">
+						<i class="fa-solid fa-trash-can"></i>
+				</div>
+			</div>
+			<div class="image_container">
+				<div class="image">
+					<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+				</div>
+				<div class="image_name">
+					<span>DSC00212.JPG</span>
+				</div>
+				<div class="image_delete">
+						<i class="fa-solid fa-trash-can"></i>
+				</div>
+			</div>
+			<div class="image_container">
+				<div class="image">
+					<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+				</div>
+				<div class="image_name">
+					<span>DSC00212.JPG</span>
+				</div>
+				<div class="image_delete">
+						<i class="fa-solid fa-trash-can"></i>
+				</div>
+			</div>
+			<div class="image_container">
+				<div class="image">
+					<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+				</div>
+				<div class="image_name">
+					<span>DSC00212.JPG</span>
+				</div>
+				<div class="image_delete">
+						<i class="fa-solid fa-trash-can"></i>
+				</div>
+			</div>
+			<div class="image_container">
+				<div class="image">
+					<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
+				</div>
+				<div class="image_name">
+					<span>DSC00212.JPG</span>
+				</div>
+				<div class="image_delete">
+						<i class="fa-solid fa-trash-can"></i>
+				</div>
 			</div>
 		</div>
-		<div class="image_container">
-			<div class="image">
-				<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
-			</div>
-			<div class="image_name">
-				<span>DSC00212.JPG</span>
-			</div>
-			<div class="image_delete">
-					<i class="fa-solid fa-trash-can"></i>
-			</div>
-		</div>
-		<div class="image_container">
-			<div class="image">
-				<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
-			</div>
-			<div class="image_name">
-				<span>DSC00212.JPG</span>
-			</div>
-			<div class="image_delete">
-					<i class="fa-solid fa-trash-can"></i>
-			</div>
-		</div>
-		<div class="image_container">
-			<div class="image">
-				<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
-			</div>
-			<div class="image_name">
-				<span>DSC00212.JPG</span>
-			</div>
-			<div class="image_delete">
-					<i class="fa-solid fa-trash-can"></i>
-			</div>
-		</div>
-		<div class="image_container">
-			<div class="image">
-				<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
-			</div>
-			<div class="image_name">
-				<span>DSC00212.JPG</span>
-			</div>
-			<div class="image_delete">
-					<i class="fa-solid fa-trash-can"></i>
-			</div>
-		</div>
-		<div class="image_container">
-			<div class="image">
-				<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
-			</div>
-			<div class="image_name">
-				<span>DSC00212.JPG</span>
-			</div>
-			<div class="image_delete">
-					<i class="fa-solid fa-trash-can"></i>
-			</div>
-		</div>
-		<div class="image_container">
-			<div class="image">
-				<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
-			</div>
-			<div class="image_name">
-				<span>DSC00212.JPG</span>
-			</div>
-			<div class="image_delete">
-					<i class="fa-solid fa-trash-can"></i>
-			</div>
-		</div>
-		<div class="image_container">
-			<div class="image">
-				<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
-			</div>
-			<div class="image_name">
-				<span>DSC00212.JPG</span>
-			</div>
-			<div class="image_delete">
-					<i class="fa-solid fa-trash-can"></i>
-			</div>
-		</div>
-		<div class="image_container">
-			<div class="image">
-				<img src="https://www.sony.pl/image/bc6d25fa6371c2899ce704a2bed7614c?fmt=png-alpha&wid=960">
-			</div>
-			<div class="image_name">
-				<span>DSC00212.JPG</span>
-			</div>
-			<div class="image_delete">
-					<i class="fa-solid fa-trash-can"></i>
-			</div>
-		</div>
-
 
 
 	</div>
