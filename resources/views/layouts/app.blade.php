b3465ce196b19b962e38c0947ed0ceeefc23f3e8
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-31 21:47:14 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,7 +121,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ route('users.create') }}" class="nav-link">
+                    <a href="{{ route('users') }}" class="nav-link">
                         <i class="fa-solid fa-users"></i>
                         <p>
                             Użytkownicy
