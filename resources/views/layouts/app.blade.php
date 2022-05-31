799f397a2648c77ce03c657f481ad3f01e9a83fa
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-31 20:53:53 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -120,6 +120,14 @@
                         </p>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a href="{{ route('users.index') }}" class="nav-link active">
+                        <i class="nav-icon fas fa-copy"></i>
+                        <p>
+                            Użytkownicy
+                        </p>
+                    </a>
+                </li>
 
             </nav>
             <!-- /.sidebar-menu -->
