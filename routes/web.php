f94af188ec6f71ee2143a3070e98c74f1a6ba4a7
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-05-31 20:48:09 +0200

    create users create files

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
     Route::resource('folders', App\Http\Controllers\FoldersController::class);
+
+    Route::resource('Users', App\Http\Controllers\UsersController::class);
 });
 
 Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'index'])->name('login');
