26462de4c68f66d306e20a42183203d278c97453
Prymex6 <bartek.prymka02@gmail.com>
2022-06-05 20:58:37 +0200

    Update show.blade.php

--- resources/views/folders/show.blade.php
+++ resources/views/folders/show.blade.php
@@ -3,9 +3,6 @@
 @section('content')
     <div class="margin-content-wraper content-wrapper">
         <div class="folder_container">
-            <div class="folder_icon">
-                <i class="fa-solid fa-folder fa-10x"></i>
-            </div>
             <span>Nazwa folderu.pl</span>
         </div>
         <div class="images_container">
