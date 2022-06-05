2f3c78cb547ba3dee9427fc8ee1c76788a74a100
Tomcio69 <tomek.litorowicz05@gmail.com>
2022-06-05 20:48:00 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::group(['middleware' => 'auth'], function () {
 
     Route::resource('folders', App\Http\Controllers\FoldersController::class);
     Route::resource('users', App\Http\Controllers\UsersController::class);
-    Route::get('folders/{id}/photos', App\Http\Controllers\UsersController::class)->name('folders.photos');
+    // Route::get('folders/{id}/photos', App\Http\Controllers\UsersController::class)->name('folders.photos');
 });
 
 Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'index'])->name('login');
