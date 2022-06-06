b63e62b94bb0060b62b5056f2e93ab1497b5a97d
Prymex6 <bartek.prymka02@gmail.com>
2022-06-06 22:16:39 +0200

    Update edit.blade.php

--- resources/views/folders/edit.blade.php
+++ resources/views/folders/edit.blade.php
@@ -34,7 +34,7 @@
                     </div>
 
                     <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Dodaj</button>
+                        <button type="submit" class="btn btn-primary">Zapisz</button>
                     </div>
                 </form>
 
