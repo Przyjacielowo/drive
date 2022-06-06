8a558c7ba6b30231643ef13b10c2c88e0b978c42
Prymex6 <bartek.prymka02@gmail.com>
2022-06-06 21:57:03 +0200

    Update show.blade.php

--- resources/views/folders/show.blade.php
+++ resources/views/folders/show.blade.php
@@ -9,7 +9,7 @@
             @foreach($photos as $photo)
             <div class="image_container">
                 <div class="image">
-                    <img src="{{ asset('storage/' . $photo->path) }}" alt="photo">
+                    <img src="{{ asset($photo->path) }}" alt="photo">
                 </div>
                 <div class="image_delete">
                     <i class="fa-solid fa-trash-can"></i>
